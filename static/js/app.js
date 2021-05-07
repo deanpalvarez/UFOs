c3f055350f3738a4e216e63b4d989acdfb9a5bc9
Dean Arnold <dean-arnold@live.com>
2021-05-07 18:32:12 -0400

    assignment

--- static/js/app.js
+++ static/js/app.js
@@ -83,7 +83,7 @@ function updateFilters() {
   }
   
   // 2. Attach an event to listen for changes to each filter
-  d3.selectAll("bg-dark test").on("change", updateFilters);
+  d3.selectAll(".test").on("change", updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
